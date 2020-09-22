8594105c0d5fbf6162c6427c2f8ff84b822f2f3d
Peter Truong <petergtruong@gmail.com>
2020-09-21 19:39:37 -0700

    Player can now shoot bullets

---
+++ Assets/Scripts/Player/BulletMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float bulletSpeed;
+    
+    void FixedUpdate()
+    {
+        transform.position += transform.up * Time.deltaTime * bulletSpeed;
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        if(collider.gameObject.CompareTag("wall")){
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,29 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Settings")]
     public float speed = 10;
+    [Header("Bullet Settings")]
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 5;
     private bool canMoveLeft = true;
     private bool canMoveRight = true;
+    private GameObject shotBullet;
 
     void Start()
     {
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && shotBullet == null){
+            shotBullet = Instantiate(bulletPrefab, transform.position + transform.up, Quaternion.identity);
+            shotBullet.GetComponent<BulletMovement>().bulletSpeed = bulletSpeed;
+        }
+    }
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.A) && canMoveLeft)
-        {
+        if (Input.GetKey(KeyCode.A) && canMoveLeft){
             transform.position -= transform.right * Time.deltaTime * speed;
         }
 
-        else if (Input.GetKey(KeyCode.D) && canMoveRight)
-        {
+        else if (Input.GetKey(KeyCode.D) && canMoveRight){
             transform.position += transform.right * Time.deltaTime * speed;            
-        }
+        }        
     }
 
     void OnTriggerEnter(Collider collider) {
-        if(collider.gameObject.CompareTag("Wall")){
+        if(collider.gameObject.CompareTag("wall")){
             if (transform.position.x < 0)
                 canMoveLeft = false;
             
@@ -36,7 +46,7 @@ public class PlayerController : MonoBehaviour
     }
 
     void OnTriggerExit(Collider collider) {
-        if(collider.gameObject.CompareTag("Wall")){
+        if(collider.gameObject.CompareTag("wall")){
             if (transform.position.x < 0)
                 canMoveLeft = true;
             
